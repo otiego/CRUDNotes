d2990c3880c10c0320b7e989099e243b0441f23a
Ronald <ronald.otiego@gmail.com>
2025-05-23 22:03:48 +0300

    Create Data class

---
+++ app/src/main/java/com/example/crudnotesapp/Note.kt
@@ -0,0 +1,8 @@
+package com.example.crudnotesapp
+
+class Note(nodeID:Int, nodeName:String, nodeDes:String) {
+    var nodeID:Int?=nodeID
+    var nodeName:String?=nodeName
+    var nodeDea:String?=nodeDes
+
+}
